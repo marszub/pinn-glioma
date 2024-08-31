130e847255a4ddb7d24f5c0beac197b4e2f3b92d
Marcin Szubert <marszub@gmail.com>
2024-08-31 23:31:45 +0200

    Add executable permissions to all .sh files

--- scripts/config/experiment1/run.sh
+++ scripts/config/experiment1/run.sh
@@ -3,8 +3,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/../../..
 
-$SCRIPT_DIR/plot-ic.sh
+mkdir -p ./tmp/experiment1/
 (time $SCRIPT_DIR/simulate.sh) &> ./tmp/experiment1/simulation-log.txt
-$SCRIPT_DIR/plot-sim.sh
 (time $SCRIPT_DIR/train.sh) &> ./tmp/experiment1/simulation-log.txt
-$SCRIPT_DIR/plot-final.sh
