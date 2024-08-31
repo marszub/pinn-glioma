709addccd9a61f9a2421fa5515783af846bc52a5
Marcin Szubert <marszub@gmail.com>
2024-09-01 01:54:48 +0200

    bugfix

--- scripts/config/experiment/run.sh
+++ scripts/config/experiment/run.sh
@@ -8,7 +8,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd $SCRIPT_DIR/../../..
 
 mkdir -p ./tmp/experiment${EXPERIMENT}/
-(time ${SCRIPT_DIR}/../experiment${EXPERIMENT}/simulate.sh) \
+(time ${SCRIPT_DIR}/simulate.sh) \
     &> ./tmp/experiment${EXPERIMENT}/simulation-log.txt
-(time $SCRIPT_DIR/../experiment${EXPERIMENT}/train.sh) \
+(time $SCRIPT_DIR/train.sh) \
     &> ./tmp/experiment${EXPERIMENT}/simulation-log.txt
